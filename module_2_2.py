895e379d82a1a6b5cf90871b29e1276fa73f9e14
MarinaNezh <nm901143@gmail.com>
2024-11-24 13:00:08 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first=55
+second=57
+third=58
+if first==second and first==third and second==third:
+    print(3)
+elif first==second or second==third or first==third:
+    print(2)
+else:
+    print(0)
+
